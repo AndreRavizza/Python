10ac63f87cb7fcb79d5f13f0d019fa84d60d5278
André Ravizza <a.ravizza@outlook.com>
2024-03-13 20:56:59 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,4 @@
+class Cliente:
+    def __init__(self, n, fone):
+        self.name = n
+        self.telefone = fone
---
+++ main.py
@@ -0,0 +1,10 @@
+class main:
+    pass
+
+print("teste")
+
+from cliente import Cliente
+
+c1 = Cliente("Nome", "123456789")
+
+print(c1.name, " e ", c1.telefone)
