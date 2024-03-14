e788cfc25c318963f6080831f4db35f09b150744
André Ravizza <a.ravizza@outlook.com>
2024-03-13 21:25:06 -0300

    Add files via upload

---
+++ Projects/defs.py
@@ -0,0 +1,3 @@
+class Calc:
+    def __init__(self, pum, pdois):
+        self.result = pum + pdois
---
+++ Projects/main.py
@@ -0,0 +1,9 @@
+class main:
+    pass
+
+
+from defs import Calc
+
+c02 = Calc(1, 3)
+
+print(c02.result)
