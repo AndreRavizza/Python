0713c527be427e6950bd4593204a4c51a1432696
André Ravizza <a.ravizza@outlook.com>
2024-03-18 21:09:33 -0300

    Update cliente.py

--- Projects/ClientsFD/cliente.py
+++ Projects/ClientsFD/cliente.py
@@ -1,4 +1,12 @@
 class Cliente:
     def __init__(self, n, fone):
-        self.nome = n
-        self.telefone = fone
+        self._nome = n
+        self._telefone = fone
+
+#Método get
+def get_nome(self):
+    return self.nome
+
+#Método set
+def set_nome(self, nome):
+    self._nome = nome
