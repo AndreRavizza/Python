84f15f178dd81d6afe5bebf891d6d794cd89f0a0
André Ravizza <a.ravizza@outlook.com>
2024-03-20 21:24:13 -0300

    Update conta.py

--- Projects/ClientsFD/conta.py
+++ Projects/ClientsFD/conta.py
@@ -1,16 +1,29 @@
 class conta:
     def __init__(self, titular, numero, saldo):
-        self.saldo = 0
+        self._saldo = 0
         self.numero = numero
         self.titular = titular
 
-        @property
-        def saldo(self):
-            return self._saldo
+    @property
+    def saldo(self):
+        return self._saldo
 
-        @saldo.setter
-        def saldo(self, saldo):
-            if (saldo<0):
-                print("O saldo não pode ser negativo")
-            else:
-                self._saldo = saldo
+    @saldo.setter
+    def saldo(self, saldo):
+        if (saldo<0):
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+
+    def saque(self, valor):
+        if(self.saldo>=valor):
+            self.saldo-=valor
+            print("Saque realizado com sucesso")
+        else:
+            print("Saldo insuficiente")
+
+    def deposita(self, valor):
+        self.saldo+=valor
+
+    def extrato(self):
+        print("Cliente", self._titular, " Saldo Atual: ", self._saldo)
