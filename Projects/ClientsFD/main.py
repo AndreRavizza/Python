a95c0c9108f2b0aef0fab1d7dd15469012a88d57
André Ravizza <a.ravizza@outlook.com>
2024-03-20 21:24:36 -0300

    Update main.py

--- Projects/ClientsFD/main.py
+++ Projects/ClientsFD/main.py
@@ -11,3 +11,10 @@ c1 = Cliente("João","114444-2222")
 conta = conta(c1.nome,6565,0)
 
 print(conta.titular," Numero: ",conta.numero, "Seu saldo: ", conta.saldo)
+
+c1 = Cliente("João","114444-2222")
+conta=conta(c1.get_nome(), 1222,0)
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
