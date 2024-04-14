ab4d00b27793f9f3a3aba5d60bd33e782091c6ca
André Ravizza <a.ravizza@outlook.com>
2024-04-14 14:29:18 -0300

    Add files via upload

---
+++ Projects/Equity Value.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+#Importando o arquivo
+
+df = pd.read_excel('Worth.xlsx')
+
+#Selecionando a coluna "Produto" e alterando-a para apenas o texto antes do "-"
+
+#.str.split divide o texto em duas partes com base no caractere
+#.str[0] seleciona apena a primeira parte do que foi selecionado
+#.str.strip() remove os espaços do texto
+
+df['Produto'] = df['Produto'].str.split('-').str[0].str.strip()
+
+print(df)
